7b62315f583e3f938ac68059c75867a6ad378812
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-09-27 17:47:31 -0400

    SQL Gernation Script Complete

--- csv_to_sql/generate_sql.rb
+++ csv_to_sql/generate_sql.rb
@@ -1,10 +1,11 @@
 require 'csv'
+require 'set'
 
 COURSES_TEMPLATE =
 	'INSERT INTO courses (dept, code, name) VALUES ("%s", %s, "%s");'
 OFFERINGS_TEMPLATE =
 	'INSERT INTO offerings (course, type, time_start, time_end, capacity)'\
-	' VALUES (%s, %s, %s, %s, %s);'
+	' VALUES (%s, "%s", %s, %s, %s);'
 
 DEPT = 0
 CODE = 1
@@ -20,30 +21,39 @@ def search_course_statement(csv_row)
 	return  "(SELECT id from courses WHERE dept = \"#{csv_row[DEPT]}\" AND code = #{csv_row[CODE]})"
 end
 
+def compound_code(csv_row)
+	return "#{csv_row[DEPT]}#{csv_row[CODE]}"
+end
+
 # Generate courses SQL
 File.open('courses.sql', 'w') { |file| 
-	file.write("DROP TABLE IF EXISTS courses;\n")
+	codes = Set.new
+	file.write("DELETE FROM courses;\n")
 	CSV.foreach('data.csv', {:col_sep => ';', :headers=>:first_row}) do |row|
-		file.write(COURSES_TEMPLATE % [row[DEPT], row[CODE], row[NAME]])
-		file.write("\n")
+		if not codes.include? compound_code row
+			codes.add compound_code row
+			file.write(COURSES_TEMPLATE % [row[DEPT], row[CODE], row[NAME]])
+			file.write("\n")
+		end
 	end
 }
 
 # Generate offerings SQL
 File.open('offerings.sql', 'w') { |file| 
-	file.write("DROP TABLE IF EXISTS offerings;\n")
+	file.write("DELETE FROM offerings;\n")
 	CSV.foreach('data.csv', {:col_sep => ';', :headers=>:first_row}) do |row|
 		course_search = search_course_statement row
-		time_start = "TIME_FORMAT(#{row[TIME_START]}, '%H%i')"
-		time_end = "TIME_FORMAT(#{row[TIME_END]}, '%H%i')"
-		file.write(OFFERINGS_TEMPLATE % [course_search, row[TYPE], time_start, time_end, row[CAPACITY]])
+		time_start = row[TIME_START].nil? ? "NULL" : "TIME_FORMAT(#{row[TIME_START]}, '%H%i')"
+		time_end = row[TIME_END].nil? ? "NULL" : "TIME_FORMAT(#{row[TIME_END]}, '%H%i')"
+		capacity = row[CAPACITY].nil? ? "NULL" : row[CAPACITY]
+		file.write(OFFERINGS_TEMPLATE % [course_search, row[TYPE], time_start, time_end, capacity])
 		file.write("\n")
 	end
 }
 
 # Generate offering_days SQL
 File.open('offering_days.sql', 'w') { |file| 
-	file.write("DROP TABLE IF EXISTS offering_days;\n")
+	file.write("DELETE FROM offering_days;\n")
 	CSV.foreach('data.csv', {:col_sep => ';', :headers=>:first_row}) do |row|
 		course_search = search_course_statement row
 		days = row[DAYS]
