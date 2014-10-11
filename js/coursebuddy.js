27d5d7222bc0d4b08fda32af4912113e4f0416f9
Ryan Seys <ryan@ryanseys.com>
2014-10-11 02:12:33 -0400

    Show list of program courses when off pattern selected

--- js/coursebuddy.js
+++ js/coursebuddy.js
@@ -1,4 +1,6 @@
 var setupselect = document.getElementById('setupselect');
+var progcourses = document.getElementById('progcourses');
+var class_selection = document.getElementById('class_selection');
 
 function get_request(url, callback) {
   var request = new XMLHttpRequest();
@@ -9,8 +11,8 @@ function get_request(url, callback) {
   request.send();
 }
 
-function get_programs(callback) {
-  get_request('programs.php', function(json_str) {
+function get_json(url, callback) {
+  get_request(url, function(json_str) {
     try {
       callback(JSON.parse(json_str));
     }
@@ -20,10 +22,46 @@ function get_programs(callback) {
   });
 }
 
-get_programs(function(data) {
-  for (var i = 0; i < data.length; i++) {
+function get_programs(callback) {
+  get_json('programs.php', callback);
+}
+
+function get_selected_program() {
+  return setupselect.options[setupselect.selectedIndex].value;
+}
+
+function get_courses(program_id, callback) {
+  var url = 'courses.php' + (program_id ? '?program=' + program_id : '');
+  get_json(url, callback);
+}
+
+function setOnPattern() {
+  class_selection.style.display = "none";
+}
+
+function setOffPattern() {
+  get_courses(get_selected_program(), function(courses) {
+    var course;
+    progcourses.innerHTML = "";
+    for (var i = 0; i < courses.length; i++) {
+      course = courses[i];
+      var li = document.createElement('li');
+      var li_input = document.createElement('input');
+      li_input.value = course.id;
+      li_input.type = "checkbox";
+      li.appendChild(li_input);
+      li.innerHTML += ' ' + course.dept + ' ' + course.code + ' - ' + course.name;
+      progcourses.appendChild(li);
+    }
+    class_selection.style.display = "";
+  });
+}
+
+get_programs(function(programs) {
+  for (var i = 0; i < programs.length; i++) {
     var el = document.createElement('option');
-    el.innerHTML = data[i].name;
+    el.innerHTML = programs[i].name;
+    el.value = programs[i].id;
     setupselect.appendChild(el);
   }
 });
