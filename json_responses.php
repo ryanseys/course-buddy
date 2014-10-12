af73f9dd6499e652948d555ee4df6bc17a273691
Andrew O'Hara <ohara.andrew314@gmail.com>
2014-10-12 17:44:19 -0400

    Improve courses api
    Add documentation to courses api
    
    update programs api
    
    Add enroll api
    
    add prereqs api
    
    Add electives api
    
    Add trailing newline
    
    improve http_args.php helper
    
    Fix error with http_args.php
    
    close db after using it
    
    jsonify api responses and remove db dependency from http_args.php
    
    Use error_json where appropriate
    
    document to_json method
    
    fix indentation
    
    fix to_json documentation
    
    really fix json_responses.php
    
    update programs.php

---
+++ json_responses.php
@@ -0,0 +1,12 @@
+<?php
+
+  // returns the json representation as {"key": value}
+  function to_json($key, $value){
+    return json_encode(array($key=>$value));
+  }
+
+  function error_json($message){
+    return to_json("error", $message);
+  }
+
+?>
