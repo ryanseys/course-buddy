d216c05625820718d4d390dab87e4d7ef7b75b87
Caleb Simpson <the.foner@gmail.com>
2014-11-19 14:14:19 -0500

    Add spinner and reduce redirect delays.

--- js/install.js
+++ js/install.js
@@ -1,11 +1,13 @@
 function install() {
-  add_message("Installing coursebuddy...Please wait, this will take a momemnt....", true);
+  add_message("Installing coursebuddy...", true);
+  add_message("<span id='spinner'><img src='img/spinner.gif' /> Please wait, this will take a momemnt....</span>", true)
   request({
     method: 'post',
     url:'install.php'
   }, function(result) {
+    document.getElementById('spinner').style.display = 'none';
     add_message(result, true);
-    setTimeout(function() { window.location = "index.html"}, 5000);
+    setTimeout(function() { window.location = "index.html"}, 2000);
   });
 }
 
@@ -16,7 +18,7 @@ function uninstall() {
     url: 'uninstall.php',
   }, function(result) {
     add_message(result, false);
-    setTimeout(function() { window.location = "install.html"}, 5000);
+    setTimeout(function() { window.location = "install.html"}, 2000);
   });
 }
 
